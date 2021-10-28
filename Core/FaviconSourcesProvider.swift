57bd5acb12f8d77e80ca1f98315199021db065dc
Gábor Szakács <gaborszakacs@users.noreply.github.com>
2021-10-28 12:12:30 +0200

    Update FaviconSourcesProvider.swift

--- Core/FaviconSourcesProvider.swift
+++ Core/FaviconSourcesProvider.swift
@@ -29,7 +29,7 @@ protocol FaviconSourcesProvider {
 
 class DefaultFaviconSourcesProvider: FaviconSourcesProvider {
     
-//testing 4
+//testing 5
     enum ImageNames: String {
         
         case appleTouch = "apple-touch-icon.png"
